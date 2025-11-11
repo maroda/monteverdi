a614365868e775e9bb87c0798461fd31f41e717a
Matt Davis <maroda@gmail.com>
2025-11-10 16:09:21 -0800

    split images try six

--- display/plugin_stub.go
+++ display/plugin_stub.go
@@ -7,7 +7,7 @@ import (
 	"log/slog"
 )
 
-func initMIDIOutput(view *View, outputLocation string) error {
+func InitMIDIOutput(view *View, outputLocation string) error {
 	slog.Warn("MIDI support not compiled in this build")
 	return fmt.Errorf("MIDI support not available")
 }
